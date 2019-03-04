611ee66ddc62aebf0c09c9e3d059649414c96c61
Zhilenas Vladas <zv@itworks24.ru>
2019-03-04 22:15:20 +0300

    Added eve-swagger support

--- run.py
+++ run.py
@@ -1,5 +1,36 @@
 from eve import Eve
+from eve_swagger import swagger, add_documentation
+
 app = Eve()
+app.register_blueprint(swagger)
+
+# required. See http://swagger.io/specification/#infoObject for details.
+app.config['SWAGGER_INFO'] = {
+    'title': 'Kopinator rest API',
+    'version': '0.0.1',
+    'description': 'an API description',
+    'termsOfService': 'my terms of service',
+    'contact': {
+        'name': 'vladas',
+        'url': 'https://github.com/vladasj'
+    },
+    'license': {
+        'name': 'BSD',
+        'url': 'https://github.com/pyeve/eve-swagger/blob/master/LICENSE',
+    },
+    'schemes': ['http', 'https'],
+}
+
+# optional. Add/Update elements in the documentation at run-time without deleting subtrees.
+add_documentation({'paths': {'/status': {'get': {'parameters': [
+    {
+        'in': 'query',
+        'name': 'foobar',
+        'required': False,
+        'description': 'special query parameter',
+        'type': 'string'
+    }]
+}}}})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
